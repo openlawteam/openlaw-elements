5fad4fca4d0440b669967e1ffaea862a452afe89
Jeremiah Trein <jeremiahtrein@gmail.com>
2019-02-14 12:54:52 +0700

    [npm-packaging] packaging for: umd, cjs, esm

--- .babelrc.js
+++ .babelrc.js
@@ -1,18 +1,53 @@
 const env = process.env.NODE_ENV;
 
-if (env === 'development') {
+const presetsCommon = [
+  "@babel/preset-react",
+  '@babel/preset-flow',
+  ['@babel/preset-env', {modules: false}],
+];
+
+if (env === 'commonjs' || env === 'esm') {
   module.exports = {
     plugins: [
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-proposal-object-rest-spread',
+      // keep the order as-is
       ["@babel/transform-runtime", {
         "regenerator": true
       }],
+      ['flow-react-proptypes', {deadCode: true, useESModules: true}],
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-object-rest-spread',
     ],
-    presets: [
-      "@babel/preset-react",
-      '@babel/preset-flow',
-      ['@babel/preset-env', {modules: false}],
+    presets: presetsCommon,
+  };
+
+  if (env === 'commonjs') {
+    module.exports.plugins.push('transform-es2015-modules-commonjs');
+  }
+}
+
+if (env === 'rollup') {
+  module.exports = {
+    comments: false,
+    plugins: [
+      ["@babel/transform-runtime", {
+        "regenerator": true
+      }],
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-object-rest-spread',
     ],
+    presets: presetsCommon,
+  };
+}
+
+if (env === 'development') {
+  module.exports = {
+    plugins: [
+      ["@babel/transform-runtime", {
+        "regenerator": true
+      }],
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-object-rest-spread',
+    ],
+    presets: presetsCommon,
   };
 }
